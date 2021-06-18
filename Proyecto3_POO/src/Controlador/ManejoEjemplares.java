f215d6bb9c779a2501b32753a7770bbbc4ab9495
Gollo <Gollo@LAPTOP-DG7IBQD4>
2021-06-18 15:33:40 -0600

    modelo-vista-controlador

---
+++ Proyecto3_POO/src/Controlador/ManejoEjemplares.java
@@ -0,0 +1,20 @@
+
+package Controlador;
+
+import Modelo.Ejemplar;
+import java.util.ArrayList;
+
+/**
+ * @author Geisel
+ */
+public class ManejoEjemplares {
+    
+    private ArrayList<Ejemplar> listaEjemplares;
+
+    public ManejoEjemplares() {
+        listaEjemplares = new ArrayList();
+    }
+    
+    
+    
+}
