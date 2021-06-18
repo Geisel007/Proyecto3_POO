e84ec8f94acb028eb3cc2b3670f531b2d4e04573
Ian Samuels <samuelsian29@gmai.com>
2021-06-18 17:43:13 -0600

    .

---
+++ Proyecto3_POO/src/Modelo/Electronica.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author IanSamuels
+ */
+public class Electronica extends NoReciclable {
+    
+    public Electronica(String pNombre, String pTratamiento, 
+            String pTiempoDescomposicion, String pImagen){
+        nombre = pNombre;
+        tratamiento = pTratamiento;
+        tiempoDescomposicion = pTiempoDescomposicion;
+        imagen = pImagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public String getTiempoDescomposicion() {
+        return tiempoDescomposicion;
+    }
+
+    public void setTiempoDescomposicion(String tiempoDescomposicion) {
+        this.tiempoDescomposicion = tiempoDescomposicion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
